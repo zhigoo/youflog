9ec61695974d5d50373a591295d2d07b6d43af30
minhao123 <minhao123@cb1cb7af-6c3c-a1c0-7acd-5018271051d7>
2011-07-17 04:04:12 +0000

    使用ajax方式删除评论和标记是否为spam

---
+++ youflog/static/admin/admin.js
@@ -0,0 +1,35 @@
+admin= {
+	decode:function(msg){
+		return eval('('+msg+')')
+	},
+	del_comment:function(id){
+		var k = this
+		$.ajax({
+			type: "POST",
+			url: "/admin/del_comment/"+id,
+			success:function(msg){
+				msg = k.decode(msg)
+				if(msg){
+					$('#comment-'+id).remove()
+				}else{
+					alert(msg[1])
+				}
+			}
+		});
+	},
+	comment_flag_spam:function(id,approve){
+		var k = this;
+		$.ajax({
+			url: "/admin/flag_spam_comment/"+id,
+			data:{'approve':approve},
+			success:function(msg){
+				msg = k.decode(msg)
+				if(msg){
+					$('#comment-'+id).remove()
+				}else{
+					alert(msg[1])
+				}
+			}
+		});
+	}
+}
